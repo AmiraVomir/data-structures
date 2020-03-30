859807e2dd8d1f8c6ae92fb1e4593ffe47d1f611
Hieu Ho <hihho@ucdavis.edu>
2020-03-29 20:35:43 -0700

    Refactored codes, attempted re-sizing limits

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,47 +1,78 @@
 var HashTable = function() {
+  var self = this;
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._size = 0;
 };
+
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var storageArr = this._storage.get(index);
-  if (storageArr === undefined) {
-    var bucket = [];
+  var bucket = [];
+  if ((storageArr) && storageArr[0] !== k) {
+    bucket.push(storageArr);
     bucket.push([k, v]);
     this._storage.set(index, bucket);
-  } else if (storageArr.length > 0) {
-    var exist = false;
-    for (var i = 0; i < storageArr.length; i++) {
-      if (storageArr[i][0] === k) {
-        storageArr[i][1] = v;
-        exist = true;
-      }
-    }
-    if (exist === false) {
-      storageArr.push([k, v]);
-    }
+  } else {
+    this._storage.set(index, [k, v]);
   }
+  // this._size++;
+  // if (this._size > this._limit * 0.75) {
+  //   this.sizeAdjust(this._limit * 2);
+  // }
 };
+
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var storageArr = this._storage.get(index);
-  for (var i = 0; i < storageArr.length; i++) {
-    if (storageArr[i][0] === k) {
-      return storageArr[i][1];
+  if (Array.isArray(storageArr[0])) {
+    for (var i = 0; i < storageArr.length; i++) {
+      if (storageArr[i][0] === k) {
+        return storageArr[i][1];
+      }
     }
+
   }
+  return storageArr[1];
 };
+
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var storageArr = this._storage.get(index);
-  for (var i = 0; i < storageArr.length; i++) {
-    var bucket = storageArr[i];
-    if (bucket[0] === k) {
-      storageArr.splice(i, 2);
-    }
+  delete storageArr[1];
+  this._size--;
+  if (this._size > this._limit * 0.25) {
+    this.sizeAdjust(this._limit * 2);
   }
   return k;
 };
+
+HashTable.prototype.sizeAdjust = function(num) {
+  //change limit
+  this._limit = num;
+  //reset size
+  this._size = 0;
+  //insert everything back in
+
+  let oldStorage = this._storage;
+  this._storage = [LimitedArray(this._limit)];
+
+  if (Array.isArray(oldStorage[0])) {
+    for (var i = 0; i < oldStorage.length; i++) {
+      var bucket = oldStorage[i];
+      if (!bucket) {
+        break;
+      }
+      for (var j = 0; j < bucket.length; j++) {
+        var tuple = bucket[i];
+        this.insert(tuple[0], tuple[1]);
+      }
+    }
+  }
+};
+
+
+// };
 /*
  * Complexity: What is the time complexity of the above functions?
  insert() complexity = linear O(n)
